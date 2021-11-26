ddd023703075ea7be5f1487df144656ccea0c441
Jakub Janků <janku.jakub.jj@gmail.com>
2021-11-26 17:18:40 +0100

    ui: add siging and groups subpages

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(title: 'Flutter Demo Home Page'),
+      home: const HomePage(),
     );
   }
 }
--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -1,9 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:mpc_demo/mpc_model.dart';
+import 'package:provider/provider.dart';
+
+class ProgressCheck extends StatelessWidget {
+  const ProgressCheck(this.value, {Key? key}) : super(key: key);
+
+  final double value;
+
+  @override
+  Widget build(BuildContext context) {
+    if (value == 1.0) return const Icon(Icons.check, color: Colors.green);
+    return SizedBox(
+      height: 24,
+      width: 24,
+      child: CircularProgressIndicator(
+        value: value,
+        strokeWidth: 2.0,
+      ),
+    );
+  }
+}
+
+class SigningSubPage extends StatelessWidget {
+  const SigningSubPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: MpcModel.files.length,
+      itemBuilder: (context, i) {
+        final file = MpcModel.files[i];
+        double progress =
+            file.round.toDouble() / file.group.protocol.signRounds;
+
+        return ListTile(
+          title: Text(file.path),
+          trailing: ProgressCheck(progress),
+          onTap: () {},
+        );
+      },
+      separatorBuilder: (context, i) => const Divider(),
+    );
+  }
+}
+
+class GroupsSubPage extends StatelessWidget {
+  const GroupsSubPage({Key? key}) : super(key: key);
+
+  String _groupInitials(Group group) {
+    return group.name
+        .split(' ')
+        .where((w) => w.isNotEmpty)
+        .take(2)
+        .map((w) => w[0])
+        .join();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: MpcModel.groups.length,
+        itemBuilder: (context, i) {
+          // final model = context.read<MpcModel>();
+          final group = MpcModel.groups[i];
+          double progress =
+              group.round.toDouble() / group.protocol.keygenRounds;
+
+          return ListTile(
+            title: Text(group.name),
+            subtitle: Text('${group.protocol.runtimeType}: '
+                '${group.threshold} out of ${group.members.length}'),
+            onTap: () {},
+            leading: CircleAvatar(
+              child: Text(
+                _groupInitials(group),
+              ),
+            ),
+            trailing: ProgressCheck(progress),
+          );
+        });
+  }
+}
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -14,16 +94,24 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
+    // final model = context.read<MpcModel>();
+
+    const pages = [
+      SigningSubPage(),
+      GroupsSubPage(),
+    ];
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text('MPC Demo'),
       ),
-      body: Center(),
+      body: Center(child: pages[_index]),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         tooltip: 'New',
         child: const Icon(Icons.add),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
