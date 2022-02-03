ad74df16ebb61fdede57dde854e5763c8e40e3d9
Jakub Janků <janku.jakub.jj@gmail.com>
2022-02-03 18:22:52 +0100

    theme: try to match it to FI MUNI colors
    Partially based on
    https://material.io/design/color/dark-theme.html#ui-application

--- lib/theme.dart
+++ lib/theme.dart
@@ -1,12 +1,24 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static final light = ThemeData(
-    primarySwatch: Colors.blue,
+  static final light = ThemeData.from(
+    colorScheme: const ColorScheme.light().copyWith(
+      primary: Colors.indigo,
+      primaryVariant: Colors.indigo[700],
+      secondary: Colors.amberAccent[200],
+      secondaryVariant: Colors.amberAccent[700],
+    ),
+  ).copyWith(
+    // necessary for avatar color, seems like a bug
+    primaryColorDark: Colors.indigo[700],
   );
 
-  static final dark = ThemeData(
-    brightness: Brightness.dark,
-    primarySwatch: Colors.teal,
+  static final dark = ThemeData.from(
+    colorScheme: const ColorScheme.dark().copyWith(
+      primary: Colors.indigo[200],
+      primaryVariant: Colors.indigo[700],
+      secondary: Colors.amberAccent[200],
+      secondaryVariant: Colors.amberAccent[200],
+    ),
   );
 }
