a88397702a87108823b4260e73edd332d0bc9700
Jakub Janků <janku.jakub.jj@gmail.com>
2022-02-04 12:19:18 +0100

    set human friendly title
    Affects title shown in the headerbar and app switcher.

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -7,6 +7,8 @@
 
 #include "flutter/generated_plugin_registrant.h"
 
+static const gchar* app_title = "Meesign";
+
 struct _MyApplication {
   GtkApplication parent_instance;
   char** dart_entrypoint_arguments;
@@ -40,11 +42,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "meesign_client");
+    gtk_header_bar_set_title(header_bar, app_title);
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "meesign_client");
+    gtk_window_set_title(window, app_title);
   }
 
   gtk_window_set_default_size(window, 1280, 720);
