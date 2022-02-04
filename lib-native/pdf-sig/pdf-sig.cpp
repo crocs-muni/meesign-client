83267ef2a97756d703cd6395cdadd1892f6cf0a9
Jakub Janků <janku.jakub.jj@gmail.com>
2022-02-04 14:35:59 +0100

    pdf-sig: make message part of signature field
    Draw the signature message inside the annotation that is
    associated with the signature field.
    This has the advantage that inside Adobe Reader, user can
    click on the message and details about the signature get displayed.

--- lib-native/pdf-sig/pdf-sig.cpp
+++ lib-native/pdf-sig/pdf-sig.cpp
@@ -7,6 +7,21 @@ using namespace PoDoFo;
 
 #define MM_IN_PDF (1000 * PODOFO_CONVERSION_CONSTANT)
 
+static PdfRect calculate_annot_pos(PdfRect page_rect)
+{
+    const double offset = 10 * MM_IN_PDF;
+    double width, height;
+    width = page_rect.GetWidth() / 2;
+    height = page_rect.GetHeight() / 4;
+
+    // origin in bottom left corner
+    PdfRect rect(
+        offset, page_rect.GetHeight() - height - offset,
+        width, height
+    );
+    return rect;
+}
+
 static PdfSignatureField draw_field(
     PdfMemDocument *document,
     const char *message
@@ -18,24 +33,22 @@ static PdfSignatureField draw_field(
     PdfAcroForm *acro_form = document->GetAcroForm(
         ePdfCreateObject, ePdfAcroFormDefaultAppearance_None);
 
-    PdfRect rect(70 * MM_IN_PDF, 10* MM_IN_PDF,
-        50 * MM_IN_PDF, 50 * MM_IN_PDF);
+    PdfRect rect = calculate_annot_pos(page->GetPageSize());
 
     PdfAnnotation *annot = page->CreateAnnotation(ePdfAnnotation_Widget, rect);
+    annot->SetFlags(ePdfAnnotationFlags_Print);
 
-    PdfSignatureField field(annot, acro_form, document);
-
+    PdfXObject x_obj(rect, document);
     PdfPainter painter;
     PdfFont* font = document->CreateFont("Courier");
 
-    painter.SetPage(page);
+    painter.SetPage(&x_obj);
     painter.SetFont(font);
-    painter.DrawMultiLineText(
-        10 * MM_IN_PDF, 10 * MM_IN_PDF,
-        100 * MM_IN_PDF, 277 * MM_IN_PDF,
-        message
-    );
+    painter.DrawMultiLineText(rect, message);
     painter.FinishPage();
+
+    PdfSignatureField field(annot, acro_form, document);
+    field.SetAppearanceStream(&x_obj);
 
     return field;
 }
