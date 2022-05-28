acb70098a1d06985b2a1438d597d9e6ded25f7b2
Jakub Janků <janku.jakub.jj@gmail.com>
2022-05-28 16:06:58 +0200

    ui: home_page: disable decline buttons
    Not supported by the server atm.

--- lib/widget/home_page.dart
+++ lib/widget/home_page.dart
@@ -255,7 +255,7 @@ class SigningSubPage extends StatelessWidget {
             ),
             OutlinedButton(
               child: const Text('DECLINE'),
-              onPressed: () => model.approveTask(task, agree: false),
+              onPressed: null,
             ),
           ];
 
@@ -413,7 +413,7 @@ class GroupsSubPage extends StatelessWidget {
               ),
               OutlinedButton(
                 child: const Text('DECLINE'),
-                onPressed: () => model.approveTask(task, agree: false),
+                onPressed: null,
               )
             ],
           );
