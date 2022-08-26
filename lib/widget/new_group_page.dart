3d783ee547f21299274f37d153f2fd065b5dfcbd
Jakub Janků <janku.jakub.jj@gmail.com>
2022-08-26 14:15:13 +0200

    fix: lint: unused imports

--- lib/widget/new_group_page.dart
+++ lib/widget/new_group_page.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:provider/provider.dart';
 
 import 'dart:io';
 
