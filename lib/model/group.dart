8ef6325c098bd115dd68000598c612c1fb4452ca
Jakub Janků <janku.jakub.jj@gmail.com>
2022-08-27 15:30:40 +0200

    model: make Group immutable

--- lib/model/group.dart
+++ lib/model/group.dart
@@ -1,23 +1,27 @@
 import 'dart:typed_data';
 
+import 'package:meta/meta.dart';
+
 import '../util/uuid.dart';
 import 'device.dart';
 
+@immutable
 class GroupBase {
-  String name;
-  List<Device> members;
-  int threshold;
+  final String name;
+  final List<Device> members;
+  final int threshold;
 
-  GroupBase(
+  const GroupBase(
     this.name,
     this.members,
     this.threshold,
   );
 }
 
+@immutable
 class Group extends GroupBase {
-  List<int> id;
-  Uint8List context;
+  final List<int> id;
+  final Uint8List context;
 
   Group(this.id, this.context, GroupBase base)
       : super(base.name, base.members, base.threshold);
