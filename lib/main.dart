b047ef044b0844968289a87213c82477eab1fc43
Jakub Janků <janku.jakub.jj@gmail.com>
2022-10-07 17:02:28 +0200

    main: add host, name command-line args

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
@@ -14,20 +17,60 @@ import 'ui/qr_reader_page.dart';
 import 'ui/registration_page.dart';
 import 'ui/search_peer_page.dart';
 
-void main() {
+void printUsage(ArgParser parser, IOSink sink) {
+  sink.writeln('Usage:');
+  sink.writeln(parser.usage);
+}
+
+void main(List<String> args) {
+  final parser = ArgParser()
+    ..addFlag(
+      'help',
+      abbr: 'h',
+      help: 'display usage information',
+      negatable: false,
+    )
+    ..addOption(
+      'host',
+      help: 'address of the server',
+    )
+    ..addOption(
+      'name',
+      help: 'name of the user',
+    );
+
+  ArgResults? results;
+  try {
+    results = parser.parse(args);
+    if (results['help']) printUsage(parser, stdout);
+  } on ArgParserException catch (e) {
+    stderr.writeln(e.message);
+    printUsage(parser, stderr);
+  }
+
   runApp(
     Provider(
       create: (_) => AppContainer(),
       child: Provider(
         create: (_) => Sync(),
-        child: const MyApp(),
+        child: MyApp(
+          prefillHost: results?['host'],
+          prefillName: results?['name'],
+        ),
       ),
     ),
   );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  final String? prefillHost;
+  final String? prefillName;
+
+  const MyApp({
+    Key? key,
+    this.prefillHost,
+    this.prefillName,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +94,10 @@ class MyApp extends StatelessWidget {
         Routes.newGroupCard: (_) => const AddCardPage(),
         Routes.newGroupQr: (_) => const QrReaderPage(),
         Routes.qrIdentity: (_) => const QrIdentityPage(),
-        Routes.registration: (_) => const RegistrationPage(),
+        Routes.registration: (_) => RegistrationPage(
+              prefillHost: prefillHost ?? 'meesign.local',
+              prefillName: prefillName ?? '',
+            ),
       },
     );
   }
