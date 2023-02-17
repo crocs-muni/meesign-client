e1e84d2752169c64d6d3b8d7a57c8553d448280c
Jakub Janků <janku.jakub.jj@gmail.com>
2023-02-17 17:28:38 +0100

    fix: util: chars: handle empty string

--- lib/util/chars.dart
+++ lib/util/chars.dart
@@ -3,5 +3,5 @@ import 'package:characters/characters.dart';
 const asciiPunctuationChars = '!"#\$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
 
 extension Initials on String {
-  String get initials => trimLeft().characters.first.toUpperCase();
+  String get initials => trimLeft().characters.take(1).toString().toUpperCase();
 }
