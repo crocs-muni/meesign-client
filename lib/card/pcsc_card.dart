4d71c1af153301132ebc98162699d7a404422038
Jakub Janků <janku.jakub.jj@gmail.com>
2024-03-01 18:38:08 +0100

    migrate to dart_pcsc from pub.dev

--- lib/card/pcsc_card.dart
+++ lib/card/pcsc_card.dart
@@ -1,24 +1,24 @@
 import 'dart:typed_data';
 
-import 'package:dart_pcsc/dart_pcsc.dart';
+import 'package:dart_pcsc/dart_pcsc.dart' as pcsc;
 import 'package:meesign_core/meesign_card.dart';
 
 import 'card.dart';
 
 class PcscCardDelegate extends Card {
-  final PcscCard _card;
+  final pcsc.Card _card;
 
   PcscCardDelegate(this._card);
 
   @override
-  Future<void> disconnect() => _card.disconnect(Disposition.leaveCard);
+  Future<void> disconnect() => _card.disconnect(pcsc.Disposition.leaveCard);
 
   @override
   Future<Uint8List> transceive(Uint8List data) => _card.transmit(data);
 }
 
 class PcscCardManager implements CardManager {
-  final PcscContext _context = PcscContext(Scope.user);
+  final pcsc.Context _context = pcsc.Context(pcsc.Scope.user);
 
   @override
   Future<void> connect() => _context.establish();
@@ -36,10 +36,10 @@ class PcscCardManager implements CardManager {
 
     List<String> withCard = await _context.waitForCard(rs).value;
 
-    PcscCard card = await _context.connect(
+    pcsc.Card card = await _context.connect(
       withCard.first,
-      ShareMode.shared,
-      Protocol.any,
+      pcsc.ShareMode.shared,
+      pcsc.Protocol.any,
     );
 
     return [PcscCardDelegate(card)];
