0d4c3880fda9c2baf62e6434f22f56f09724f88f
Jakub Janků <janku.jakub.jj@gmail.com>
2024-04-28 16:35:56 +0200

    move WeightedAvatar to a separate file

--- lib/ui/new_group_page.dart
+++ lib/ui/new_group_page.dart
@@ -1,7 +1,6 @@
 import 'dart:math';
 
 import 'package:collection/collection.dart';
-import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:meesign_core/meesign_model.dart';
@@ -10,6 +9,7 @@ import 'dart:io';
 
 import '../routes.dart';
 import '../util/chars.dart';
+import '../widget/weighted_avatar.dart';
 
 class OptionTile extends StatelessWidget {
   final String title;
@@ -114,55 +114,6 @@ class NumberInput extends StatelessWidget {
         IconButton(
           icon: const Icon(Icons.chevron_right),
           onPressed: onUpdate != null ? () => onUpdate!(value + 1) : null,
-        ),
-      ],
-    );
-  }
-}
-
-class WeightedAvatar extends StatelessWidget {
-  final int index;
-  final List<int> weights;
-  final Widget? child;
-
-  const WeightedAvatar({
-    super.key,
-    required this.index,
-    required this.weights,
-    this.child,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: AlignmentDirectional.center,
-      children: [
-        SizedBox.square(
-          dimension: 32,
-          child: CircleAvatar(
-            child: child,
-          ),
-        ),
-        SizedBox.square(
-          dimension: 40,
-          child: PieChart(
-            PieChartData(
-              startDegreeOffset: -90,
-              sectionsSpace: 2,
-              sections: [
-                for (final (j, weight) in weights.indexed)
-                  PieChartSectionData(
-                    radius: 2,
-                    value: weight.toDouble(),
-                    showTitle: false,
-                    color: Theme.of(context)
-                        .colorScheme
-                        .primary
-                        .withOpacity(j == index ? 1 : .2),
-                  )
-              ],
-            ),
-          ),
         ),
       ],
     );
---
+++ lib/widget/weighted_avatar.dart
@@ -0,0 +1,51 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class WeightedAvatar extends StatelessWidget {
+  final int index;
+  final List<int> weights;
+  final Widget? child;
+
+  const WeightedAvatar({
+    super.key,
+    required this.index,
+    required this.weights,
+    this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: [
+        SizedBox.square(
+          dimension: 32,
+          child: CircleAvatar(
+            child: child,
+          ),
+        ),
+        SizedBox.square(
+          dimension: 40,
+          child: PieChart(
+            PieChartData(
+              startDegreeOffset: -90,
+              sectionsSpace: 2,
+              sections: [
+                for (final (j, weight) in weights.indexed)
+                  PieChartSectionData(
+                    radius: 2,
+                    value: weight.toDouble(),
+                    showTitle: false,
+                    color: Theme.of(context)
+                        .colorScheme
+                        .primary
+                        .withOpacity(j == index ? 1 : .2),
+                  )
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
