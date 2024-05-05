052f1bfdd88f21e5bebd616d70b02a35019985ee
Jakub Janků <janku.jakub.jj@gmail.com>
2024-05-05 14:55:54 +0200

    core: support: return Version object

--- meesign_core/lib/src/data/support_services.dart
+++ meesign_core/lib/src/data/support_services.dart
@@ -1,5 +1,6 @@
 import 'package:meesign_network/grpc.dart' as rpc;
 import 'package:meesign_network/meesign_network.dart';
+import 'package:pub_semver/pub_semver.dart';
 
 import '../util/uuid.dart';
 import 'network_dispatcher.dart';
@@ -9,10 +10,10 @@ class SupportServices {
 
   SupportServices(this._dispatcher);
 
-  Future<String> getVersion([Uuid? did]) async {
+  Future<Version> getVersion([Uuid? did]) async {
     final info = await (did != null ? _dispatcher[did] : _dispatcher.unauth)
         .getServerInfo(ServerInfoRequest());
-    return info.version;
+    return Version.parse(info.version);
   }
 
   Future<void> log(Uuid? did, String message) =>
