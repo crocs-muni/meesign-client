5b038446975ea8d84576bb25b451ea5b49886ae5
Jakub Janků <janku.jakub.jj@gmail.com>
2024-05-05 22:39:55 +0200

    widget: add actions to WarningBanner

--- lib/widget/warning_banner.dart
+++ lib/widget/warning_banner.dart
@@ -4,22 +4,44 @@ import 'package:material_symbols_icons/symbols.dart';
 class WarningBanner extends StatelessWidget {
   final String title;
   final String text;
+  final List<Widget> actions;
 
   const WarningBanner({
     super.key,
     required this.title,
     required this.text,
+    this.actions = const [],
   });
 
   @override
   Widget build(BuildContext context) {
     final colorScheme = Theme.of(context).colorScheme;
 
+    // FIXME: any better way?
+    final buttonTheme = Theme.of(context).copyWith(
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: colorScheme.onErrorContainer,
+        ),
+      ),
+      outlinedButtonTheme: OutlinedButtonThemeData(
+        style: OutlinedButton.styleFrom(
+          foregroundColor: colorScheme.onErrorContainer,
+        ),
+      ),
+      filledButtonTheme: FilledButtonThemeData(
+        style: FilledButton.styleFrom(
+          foregroundColor: colorScheme.onError,
+          backgroundColor: colorScheme.error,
+        ),
+      ),
+    );
+
     return Container(
       color: colorScheme.errorContainer,
       padding: const EdgeInsets.all(16),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Row(
             children: [
@@ -39,6 +61,18 @@ class WarningBanner extends StatelessWidget {
           ),
           const SizedBox(height: 8),
           Text(text),
+          if (actions.isNotEmpty) ...[
+            const SizedBox(height: 16),
+            Theme(
+              data: buttonTheme,
+              child: Wrap(
+                alignment: WrapAlignment.end,
+                spacing: 8,
+                runSpacing: 8,
+                children: actions,
+              ),
+            ),
+          ]
         ],
       ),
     );
