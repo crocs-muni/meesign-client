840a20256bdeb98bc4300bfb25200c660783c778
Robin Chmelík <54114376+Ojin13@users.noreply.github.com>
2025-03-12 16:36:32 +0100

    Fixed linter errors

---
+++ lib/ui_constants.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: constant_identifier_names
+
+// Standardized gap sizes
+const SMALL_GAP = 8.0;
+const MEDIUM_GAP = 16.0;
+const LARGE_GAP = 24.0;
+const XLARGE_GAP = 32.0;
+
+// Standardized border radiuses
+const SMALL_BORDER_RADIUS = 8.0;
+const MEDIUM_BORDER_RADIUS = 16.0;
+const LARGE_BORDER_RADIUS = 32.0;
+
+// Standardized padding
+const double SMALL_PADDING = 8.0;
+const double MEDIUM_PADDING = 16.0;
+const double LARGE_PADDING = 24.0;
+const double XLARGE_PADDING = 32.0;
