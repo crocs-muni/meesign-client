520c0b7c379b3f677f1501fb8f7ba539bb4baadb
Robin Chmelík <54114376+Ojin13@users.noreply.github.com>
2025-03-27 11:35:22 +0100

    Added device deletion and fade page transition

--- lib/pages/device_settings_page.dart
+++ lib/pages/device_settings_page.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../app_container.dart';
 import '../templates/default_page_template.dart';
 import '../ui_constants.dart';
+import '../util/fade_black_page_transition.dart';
 import '../view_model/app_view_model.dart';
 import '../widget/confirmation_dialog.dart';
+import 'register_page.dart';
 
 class DeviceSettingsPage extends StatefulWidget {
   const DeviceSettingsPage({super.key});
@@ -52,112 +55,11 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Text(
-            "Device name",
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          SizedBox(height: SMALL_GAP),
-          Consumer<AppViewModel>(builder: (context, model, child) {
-            return Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  enabled: false,
-                  readOnly: true,
-                  controller: _nameController,
-                  focusNode: _nameControllerFocus,
-                  onChanged: (_) => setState(() {}),
-                  decoration: InputDecoration(
-                    suffixIcon: _nameController.text == "" ||
-                            !_nameControllerFocus.hasFocus
-                        ? null
-                        : IconButton(
-                            // Icon to
-                            icon: const Icon(Icons.clear),
-                            onPressed: () {
-                              setState(() {
-                                _nameController.clear();
-                              });
-                            },
-                          ),
-                    filled: true,
-                    hintText: 'Name to identify yourself',
-                    hintStyle: TextStyle(
-                      color: Theme.of(context).colorScheme.outline,
-                    ),
-                    border: const OutlineInputBorder(),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Theme.of(context).colorScheme.primary,
-                          width: 0),
-                    ),
-                    errorText: null,
-                  ),
-                ),
-              ],
-            );
-          }),
+          _buildDeviceNameSection(),
           SizedBox(height: XLARGE_GAP),
-          Text(
-            "Change server",
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          SizedBox(height: SMALL_GAP),
-          Text(
-            "This will take you back to the registration screen where you can change the server or register a new device.",
-            style: TextStyle(color: Theme.of(context).colorScheme.outline),
-          ),
-          SizedBox(height: MEDIUM_GAP),
-          FilledButton.icon(
-            onPressed: () {
-              _showDeleteDialog();
-            },
-            label: Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: Text('Change server'),
-            ),
-            icon: Icon(Icons.sync),
-            style: ButtonStyle(
-              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-          ),
+          _buildChangeServerSection(),
           SizedBox(height: XLARGE_GAP * 2),
-          Text(
-            "Danger zone",
-            style: Theme.of(context).textTheme.bodyLarge,
-          ),
-          SizedBox(height: SMALL_GAP),
-          Text(
-            "This action will effectively delete your device and all associated data. After deletion, you will need to re-register your device to continue using the app.",
-            style: TextStyle(color: Theme.of(context).colorScheme.outline),
-          ),
-          SizedBox(height: MEDIUM_GAP),
-          FilledButton.icon(
-            onPressed: () {
-              _showDeleteDialog();
-            },
-            label: Padding(
-              padding: EdgeInsets.symmetric(vertical: 15),
-              child: Text('Delete device',
-                  style: TextStyle(
-                      color: Theme.of(context).colorScheme.onErrorContainer)),
-            ),
-            icon: Icon(Icons.delete,
-                color: Theme.of(context).colorScheme.onErrorContainer),
-            style: ButtonStyle(
-              backgroundColor: WidgetStateProperty.all<Color>(
-                  Theme.of(context).colorScheme.errorContainer),
-              shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-            ),
-          ),
+          _buildDangerZone()
         ],
       ),
     );
@@ -169,7 +71,144 @@ class _DeviceSettingsPageState extends State<DeviceSettingsPage> {
       'Confirm deletion',
       'Are you sure you want to delete this device?',
       'Delete',
-      () {},
+      () async {
+        final appContainer = context.read<AppContainer>();
+        await appContainer.recreate(deleteData: true);
+
+        if (mounted) {
+          Navigator.of(context, rootNavigator: true).pushAndRemoveUntil(
+            FadeBlackPageTransition.fadeBlack(destination: RegisterPage()),
+            (route) => false,
+          );
+        }
+      },
+    );
+  }
+
+  Widget _buildDeviceNameSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Device name",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        SizedBox(height: SMALL_GAP),
+        Consumer<AppViewModel>(builder: (context, model, child) {
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                enabled: false,
+                readOnly: true,
+                controller: _nameController,
+                focusNode: _nameControllerFocus,
+                onChanged: (_) => setState(() {}),
+                decoration: InputDecoration(
+                  suffixIcon: _nameController.text == "" ||
+                          !_nameControllerFocus.hasFocus
+                      ? null
+                      : IconButton(
+                          // Icon to
+                          icon: const Icon(Icons.clear),
+                          onPressed: () {
+                            setState(() {
+                              _nameController.clear();
+                            });
+                          },
+                        ),
+                  filled: true,
+                  hintText: 'Name to identify yourself',
+                  hintStyle: TextStyle(
+                    color: Theme.of(context).colorScheme.outline,
+                  ),
+                  border: const OutlineInputBorder(),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                        color: Theme.of(context).colorScheme.primary, width: 0),
+                  ),
+                  errorText: null,
+                ),
+              ),
+            ],
+          );
+        }),
+      ],
+    );
+  }
+
+  Widget _buildChangeServerSection() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Change server",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        SizedBox(height: SMALL_GAP),
+        Text(
+          "This will take you back to the registration screen where you can change the server or register a new device.",
+          style: TextStyle(color: Theme.of(context).colorScheme.outline),
+        ),
+        SizedBox(height: MEDIUM_GAP),
+        FilledButton.icon(
+          onPressed: () {
+            _showDeleteDialog();
+          },
+          label: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            child: Text('Change server'),
+          ),
+          icon: Icon(Icons.sync),
+          style: ButtonStyle(
+            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDangerZone() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Danger zone",
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        SizedBox(height: SMALL_GAP),
+        Text(
+          "This action will effectively delete your device and all associated data. After deletion, you will need to re-register your device to continue using the app.",
+          style: TextStyle(color: Theme.of(context).colorScheme.outline),
+        ),
+        SizedBox(height: MEDIUM_GAP),
+        FilledButton.icon(
+          onPressed: () {
+            _showDeleteDialog();
+          },
+          label: Padding(
+            padding: EdgeInsets.symmetric(vertical: 15),
+            child: Text('Delete device',
+                style: TextStyle(
+                    color: Theme.of(context).colorScheme.onErrorContainer)),
+          ),
+          icon: Icon(Icons.delete,
+              color: Theme.of(context).colorScheme.onErrorContainer),
+          style: ButtonStyle(
+            backgroundColor: WidgetStateProperty.all<Color>(
+                Theme.of(context).colorScheme.errorContainer),
+            shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
